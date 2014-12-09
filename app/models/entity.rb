72f7e442b54e4ce11c7c94967bb707b6722a48fa
IsNology <glenn@isnology.com>
2014-12-10 02:38:19 +1100

    Initial commit

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: entities
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Entity < ActiveRecord::Base
   belongs_to :warehouses
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141121095434) do
+ActiveRecord::Schema.define(version: 20141209075347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 20141121095434) do
   create_table "countries", force: true do |t|
     t.string   "name"
     t.string   "code"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "entities", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
